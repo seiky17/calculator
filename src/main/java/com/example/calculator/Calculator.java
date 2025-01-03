ef18f43971ef78ab25e716726c7da32ffd33585a
sugeong <hsk5997@gmail.com>
2025-01-03 11:27:49 +0900

    exit if문 변경

--- src/main/java/com/example/calculator/Calculator.java
+++ src/main/java/com/example/calculator/Calculator.java
@@ -34,7 +34,7 @@ public class Calculator {
 
             System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
             String str = sc.next();
-            if(Objects.equals(str, "exit")) {
+            if(str.equals("exit")) {
                 break;
             }
         }
